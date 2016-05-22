7a7d802bfcb460bca7f6f543d275ea354e1f3bbb
guillaume.saupin@gmail.com <guillaume.saupin@gmail.com>
2016-05-22 22:51:50 +0200

    * work in progress

---
+++ main-complex.js
@@ -0,0 +1,34 @@
+var h = require('virtual-dom/h')
+var createElement = require('virtual-dom/create-element');
+var CounterModel = require('./counter');
+var ClickCounterModel = require('./click-counter');
+var MyAppModel = require('./my-app');
+
+
+// Dispatch the event.
+function render (myApp) {
+    var tree = h('div');
+    var rootNode = createElement(tree);
+    rootNode.appendChild(myApp.render());
+    return rootNode;
+}
+
+var model1 = new CounterModel();
+//var model2 = new CounterModel();
+//var model3 = new ClickCounterModel();
+
+
+//var myApp = new MyAppModel();
+//myApp.setState({model1:model1, model2:model2, model3:model3})
+
+//var rootNode = render(myApp, model2, model3);
+//var rootNode = render(myApp);
+var rootNode = model1.render();
+
+document.body.appendChild(rootNode);
+
+//model1.forward("increment", model3);
+//model1.forward("decrement", model3);
+
+//model2.forward("increment", model3);
+//model2.forward("decrement", model3);
